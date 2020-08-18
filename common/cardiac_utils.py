87db6194c566a54336683463dd6622d4efcb9f65
manigalati <32110860+manigalati@users.noreply.github.com>
2020-08-18 14:59:57 +0200

    Update cardiac_utils.py

--- common/cardiac_utils.py
+++ common/cardiac_utils.py
@@ -606,7 +606,7 @@ def extract_myocardial_contour(seg_name, contour_name_stem, part=None, three_sli
         lv_centre = np.dot(affine, np.array([cx, cy, z, 1]))[:3]
 
         # Extract epicardial contour
-        _, contours, _ = cv2.findContours(cv2.inRange(epi, 1, 1), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        contours, _ = cv2.findContours(cv2.inRange(epi, 1, 1), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
         epi_contour = contours[0][:, 0, :]
         epi_contour = approximate_contour(epi_contour, periodic=True)
 
@@ -660,7 +660,7 @@ def extract_myocardial_contour(seg_name, contour_name_stem, part=None, three_sli
         # Extract endocardial contour
         # Note: cv2 considers an input image as a Y x X array, which is different
         # from nibabel which assumes a X x Y array.
-        _, contours, _ = cv2.findContours(cv2.inRange(endo, 1, 1), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        contours, _ = cv2.findContours(cv2.inRange(endo, 1, 1), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
         endo_contour = contours[0][:, 0, :]
         endo_contour = approximate_contour(endo_contour, periodic=True)
 
@@ -1181,11 +1181,11 @@ def extract_la_myocardial_contour(seg_la_name, seg_sa_name, contour_name):
     # Extract endocardial contour
     # Note: cv2 considers an input image as a Y x X array, which is different
     # from nibabel which assumes a X x Y array.
-    _, contours, _ = cv2.findContours(cv2.inRange(endo, 1, 1), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    contours, _ = cv2.findContours(cv2.inRange(endo, 1, 1), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     endo_contour = contours[0][:, 0, :]
 
     # Extract epicardial contour
-    _, contours, _ = cv2.findContours(cv2.inRange(epi, 1, 1), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    contours, _ = cv2.findContours(cv2.inRange(epi, 1, 1), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     epi_contour = contours[0][:, 0, :]
 
     # Record the points located on the mitral valve plane.
